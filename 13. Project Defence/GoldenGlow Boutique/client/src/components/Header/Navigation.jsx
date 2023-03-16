bdcfd798adb36ba943f082dc16f3c242c078f61f
yoantodorov <todorovyoan075@gmail.com>
2023-03-16 11:31:45 +0200

    Added separating line in navigation

--- 13. Project Defence/GoldenGlow Boutique/client/src/components/Header/Navigation.jsx
+++ 13. Project Defence/GoldenGlow Boutique/client/src/components/Header/Navigation.jsx
@@ -7,25 +7,56 @@ export const Navigation = () => {
         <nav className={styles['nav']}>
             <ul className={styles['nav-btn-list']}>
                 <li className={styles['nav-btn-list-item']}>
-                    <NavLink className={({isActive}) => isActive ? styles['nav-btn-li-link-selected'] : styles['nav-btn-li-link']} to="/">Home</NavLink>
+                    <NavLink 
+                        className={({isActive}) => isActive ? styles['nav-btn-li-link-selected'] : styles['nav-btn-li-link']} 
+                        to="/">
+                            Home
+                    </NavLink>
                 </li>
                 <li className={styles['nav-btn-list-item']}>
-                    <NavLink className={({isActive}) => isActive ? styles['nav-btn-li-link-selected'] : styles['nav-btn-li-link']} to="/about">About</NavLink>
+                    <NavLink 
+                        className={({isActive}) => isActive ? styles['nav-btn-li-link-selected'] : styles['nav-btn-li-link']} 
+                        to="/about">
+                            About
+                    </NavLink>
                 </li>
                 <li className={styles['nav-btn-list-item']}>
-                    <NavLink className={({isActive}) => isActive ? styles['nav-btn-li-link-selected'] : styles['nav-btn-li-link']} to="/contact">Contact</NavLink>
+                    <NavLink 
+                        className={({isActive}) => isActive ? styles['nav-btn-li-link-selected'] : styles['nav-btn-li-link']} 
+                        to="/contact">
+                            Contact
+                    </NavLink>
                 </li>
                 <li className={styles['nav-btn-list-item']}>
-                    <NavLink className={({isActive}) => isActive ? styles['nav-btn-li-link-selected'] : styles['nav-btn-li-link']} to="/catalog">Catalog</NavLink>
+                    <NavLink 
+                        className={({isActive}) => isActive ? styles['nav-btn-li-link-selected'] : styles['nav-btn-li-link']} 
+                        to="/catalog">
+                            Catalog
+                    </NavLink>
+                </li>
+                <li>
+                    <div className={styles['v-line']}></div>
                 </li>
                 <li className={styles['nav-btn-list-item']}>
-                    <NavLink className={({isActive}) => isActive ? styles['nav-btn-li-link-selected'] : styles['nav-btn-li-link']} to="/login">Login</NavLink>
+                    <NavLink 
+                        className={({isActive}) => isActive ? styles['nav-btn-li-link-selected'] : styles['nav-btn-li-link']} 
+                        to="/login">
+                            Login
+                    </NavLink>
                 </li>
                 <li className={styles['nav-btn-list-item']}>
-                    <NavLink className={({isActive}) => isActive ? styles['nav-btn-li-link-selected'] : styles['nav-btn-li-link']} to="/register">Register</NavLink>
+                    <NavLink 
+                        className={({isActive}) => isActive ? styles['nav-btn-li-link-selected'] : styles['nav-btn-li-link']} 
+                        to="/register">
+                            Register
+                    </NavLink>
                 </li>
                 {/* <li className={styles['nav-btn-list-item']}>
-                    <NavLink className={({isActive}) => isActive ? styles['nav-btn-li-link-selected'] : styles['nav-btn-li-link']} to="/logout">Logout</NavLink>
+                    <NavLink 
+                        className={({isActive}) => isActive ? styles['nav-btn-li-link-selected'] : styles['nav-btn-li-link']} 
+                        to="/logout">
+                            Logout
+                </NavLink>
                 </li> */}
             </ul>
         </nav>
