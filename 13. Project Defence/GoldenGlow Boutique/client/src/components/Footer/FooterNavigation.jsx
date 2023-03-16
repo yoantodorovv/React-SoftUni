86be44a2b0581386159eb1cf85771b8df0d50892
yoantodorov <todorovyoan075@gmail.com>
2023-03-16 10:30:29 +0200

    Refactored code

--- 13. Project Defence/GoldenGlow Boutique/client/src/components/Footer/FooterNavigation.jsx
+++ 13. Project Defence/GoldenGlow Boutique/client/src/components/Footer/FooterNavigation.jsx
@@ -1,38 +1,62 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { NavLink } from 'react-router-dom'
 
 import { FooterNavigationElement } from "./FooterNavigationElement";
 
 import styles from "./Footer.module.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
-export const FooterNavigation = () => {
-    const pagesRoutes = {
+const routes = {
+    'pages': {
         'Home': '/',
         'About': '/about',
         'Contact': '/contact',
         'Catalog': '/catalog',
         'Women': 'catalog/women',
         'Men': 'catalog/men',
-    }
-
-    const profileRoutes = {
+    },
+    'profiles': {
         'Login': '/login',
         'Register': '/register',
         'Logout': '/logout',
     }
+}
 
+export const FooterNavigation = () => {
     return (
         <div className={styles['footer-nav']}>
-            <FooterNavigationElement title="Pages" options={pagesRoutes}/>
-            <FooterNavigationElement title="Your Profile" options={profileRoutes} />
-            <div className="footer-map">
-                <h2 className={styles['footer-titles']}>Follow Us On</h2>
+            {/* <FooterNavigationElement title="Pages" options={pagesRoutes}/> */}
+            <FooterNavigationElement title="Pages">
+                <div className={styles['options-wrapper']}>
+                    {Object.entries(routes.pages)
+                        .map(x => 
+                            <NavLink 
+                                key={`${x[0]}-${x[1]}`} 
+                                to={x[1]} 
+                                className={({ isActive }) => isActive ? styles['footer-options-selected'] : styles['footer-options']}>
+                                    {x[0]}
+                            </NavLink>)}
+                </div>
+            </FooterNavigationElement>
+            <FooterNavigationElement title="Your Profile">
+                <div className={styles['options-wrapper']}>
+                    {Object.entries(routes.profiles)
+                        .map(x => 
+                            <NavLink
+                                key={`${x[0]}-${x[1]}`} 
+                                to={x[1]} 
+                                className={({ isActive }) => isActive ? styles['footer-options-selected'] : styles['footer-options']}>
+                                    {x[0]}
+                            </NavLink>)}
+                </div>
+            </FooterNavigationElement>
+            <FooterNavigationElement title="Follow Us On">
                 <div className={styles['icon-wrapper']}>
                     <FontAwesomeIcon className={styles['icon']} icon={faInstagram} size="2x" />
                     <FontAwesomeIcon className={styles['icon']} icon={faTwitter} size="2x" />
                     <FontAwesomeIcon className={styles['icon']} icon={faFacebook} size="2x" />
                 </div>
-            </div>
+            </FooterNavigationElement>
         </div>
     );
 }
--- 13. Project Defence/GoldenGlow Boutique/client/src/components/Footer/FooterNavigationElement.jsx
+++ 13. Project Defence/GoldenGlow Boutique/client/src/components/Footer/FooterNavigationElement.jsx
@@ -1,17 +1,13 @@
-import { NavLink } from 'react-router-dom'
-
 import styles from "./Footer.module.scss"
 
 export const FooterNavigationElement = ({
+    children,
     title,
-    options
 }) => {
     return (
         <div className={styles['footer-map']}>
             <h2 className={styles['footer-titles']}>{title}</h2>
-            <div className={styles['options-wrapper']}>
-                {Object.entries(options).map(x => <NavLink key={`${x[0]}-${x[1]}`} to={x[1]} className={({isActive}) => isActive ? styles['footer-options-selected'] : styles['footer-options']}>{x[0]}</NavLink>)}
-            </div>
+            {children}
         </div>
     );
 }
